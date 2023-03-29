f69e96c9d023464f80d7e83903b4af07fca8c498
MGohnTrial <129204781+MGohnTrial@users.noreply.github.com>
2023-03-29 15:06:10 -0400

    Update streamlit_app.py
    introduce a try-catch block for better error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,16 +27,18 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-#take the json response and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-#output the normalized version to the screen
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #This is to prevent Snowflake from running while troubleshooting
 streamlit.stop()
